2baf4a90710667a5501a5778105febce428e068b
JohnSmith114 <kientuong@gmail.com>
2020-03-12 15:10:36 +0100

    fix: Fix a bug with Schema Tree generation. Update Readme and renamed Parser.py. Add new test schema for XML

--- similarityflooding/XmlParser.py
+++ similarityflooding/XmlParser.py
@@ -2,7 +2,17 @@ import xml.etree.ElementTree as et
 from collections import namedtuple
 
 class SchemaNode:
-    # Represents a node of the schema tree
+    """Node for the Schema Tree.
+    
+    A Schema Tree is a n-ary tree which represents a hierachical data set.
+    A Schema Tree should be created only via the parsing functions (e.g. parse_xml())
+
+    Attributes:
+        tag (str): The tag associated to the node (e.g XML tag)
+        attrib: The set of string attributes associated to the tag
+        children: The dict of all children nodes, indexed by their tag
+        father: Reference to the father of tha node
+    """
 
     def __init__(self, tag, attrib = None):
         self.tag = tag
@@ -11,28 +21,44 @@ class SchemaNode:
         self.father = None
 
     def __repr__(self):
-        s = f"""SchemaNode(tag = {self.tag}, attrib = {list(self.attrib)},""" +\
-                    f"""children = {[tag for tag in self.children.keys()]}, father = {self.father.tag if self.father else None})"""
+        s = f"""SchemaNode(tag = {self.tag},""" +\
+                    f"""attrib = {list(self.attrib)},""" +\
+                    f"""children = {[tag for tag in self.children.keys()]},""" +\
+                    f"""father = {self.father.tag if self.father else None})"""
         return s
 
-    def addChild(self, child):
+    def add_child(self, child):
+        """Adds child to the children dict of self and updates its father reference"""
         self.children.update({child.tag: child})
         child.father = self
 
     @property
-    def childrenTags(self):
+    def children_tags(self):
         return self.children.keys()
 
-    @staticmethod
-    def printTree(root):
-        print(root)
-        for tag in sorted(root.children.keys()):
-            SchemaNode.printTree(root.children[tag])
+
+def print_tree(root):
+    """Print all nodes of the Schema Tree in pre-order
+
+    Args:
+        root (str): the root node of the tree to be printed
+    """
+    print(root)
+    for tag in sorted(root.children.keys()):
+        SchemaNode.print_tree(root.children[tag])
 
 
 def parse_xml(file_path):
+    """Return the schema tree for the xml file located in file_path
+
+    Args:
+        file_path (str): Relative file path to the xml file
+    """
+
     PathNode = namedtuple('PathNode', ['tag', 'attrib'])
-    def leafPaths(root, path = []):
+
+
+    def leaf_paths(root, path=[]):
         # Generator of all root-leaf paths
         path = list(path)
         path.append(PathNode(root.tag, set(root.keys())))
@@ -41,25 +67,31 @@ def parse_xml(file_path):
             yield path
         else:
             for child in root:
-                yield from leafPaths(child, path)
+                yield from leaf_paths(child, path)
 
     xml_tree = et.parse(file_path)
     root = xml_tree.getroot()
 
-    schema_tree = SchemaNode(root.tag, attrib = set(root.keys()))
-    g = leafPaths(root)
+    schema_tree = SchemaNode(root.tag, attrib=set(root.keys()))
+    g = leaf_paths(root)
 
     for path in g:
         curr = schema_tree
         for node in path[1:]:
-            if node.tag not in curr.childrenTags:
+            if node.tag not in curr.children_tags:
                 succ = SchemaNode(node.tag, attrib = node.attrib)
-                curr.addChild(succ)
+                curr.add_child(succ)
             else:
                 succ = curr.children[node.tag]
                 succ.attrib = succ.attrib.union(node.attrib)
-            succ = curr
-    SchemaNode.printTree(schema_tree)
+            curr = succ
+
+    return schema_tree
+
+
+def schema_tree2Graph():
+    return
+
 
 if __name__ == "__main__":
     parse_xml('test.xml')
