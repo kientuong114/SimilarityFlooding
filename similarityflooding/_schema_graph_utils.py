8ad939436a528944067271de164927d6c757907f
Stefan Djokovic <ste.djokovic@gmail.com>
2020-04-12 10:30:02 +0200

    style: PEP 8: two blank lines

--- similarityflooding/_schema_graph_utils.py
+++ similarityflooding/_schema_graph_utils.py
@@ -1,11 +1,13 @@
 import networkx as nx
 import pylab
 
+
 def OID_generator(char = 'a', start_num = 1):
     n = start_num
     while True:
         yield char + str(n)
         n += 1
+
 
 def schema_graph_draw(G, title='title'):
     pos = nx.spring_layout(G, k=3)
