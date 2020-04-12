96f7c1c9e986b8da35ac3c7df90143cf3022725a
Stefan Djokovic <ste.djokovic@gmail.com>
2020-04-12 10:30:48 +0200

    refactor: added dict of the elements of the schema, now elements are unique in the graph.

--- similarityflooding/sql_parser.py
+++ similarityflooding/sql_parser.py
@@ -1,5 +1,4 @@
 import re
-import pprint
 import _schema_graph_utils as sgu
 
 def parse_sql(file_path):
@@ -13,12 +12,6 @@ def parse_sql(file_path):
 
 
 def sql_ddl2graph(data, G):
-    import networkx as nx
-
-    # todo: index nodes for now are connected to each other only for 'column' and 'SQLType' relations, so
-    #   have to add connections between nodes that have destination nodes with the same characteristics
-    # todo: add weights
-    # todo: apply other libraries or external software for more beautiful graph drawings
 
     G.add_node('Table')
     G.add_node('Column')
@@ -27,40 +20,57 @@ def sql_ddl2graph(data, G):
     oid = sgu.OID_generator(char='&')
 
     query_elements = []
+    print(data)
     for table in data:
         temp = table.split(',')
         temp[-1] = temp[-1][:-2]    # Fixing open parenthesis of last element
         query_elements.append(temp)
+    print(query_elements)
 
-    creation_table = re.compile(r'\s*(CREATE TABLE )(?P<tableName>\w*)\s*(\()\s(?P<nameColumn>\w*)\s(?P<SQLType>\S+)\s*(?P<other>.*)')
-    creation_column = re.compile(r'\s*(?P<nameColumn>\w*)\s(?P<SQLType>\S+)\s*(?P<other>.*)')
+    creation_table = re.compile(r'\s*(CREATE TABLE )(?P<tableName>\w*)\s*(\()\s(?P<nameColumn>\w*)\s(?P<SQLtype>\S+)\s*(?P<other>.*)')
+    creation_column = re.compile(r'\s*(?P<nameColumn>\w*)\s(?P<SQLtype>\S+)\s*(?P<other>.*)')
 
+    elements = {}
     for tables in query_elements:
-        for elements in tables:
-            match = creation_table.match(elements)
+        for rows in tables:
+            match = creation_table.match(rows)
             if match is not None:
-                # Adding tableName and its components
-                table_name = match.group('tableName')
-                name_elem = next(oid)
-                G.add_edge(name_elem, table_name, title='name')
-                G.add_edge(name_elem, 'Table', title='type')
+                # adding the new table
+                name_table = match.group('tableName')
+                oid_table = next(oid)
+                key_table = str(name_table) + " ; " + str("Table")
+                elements[key_table] = oid_table
+                G.add_edge(oid_table, name_table, title='name')
+                G.add_edge(oid_table, 'Table', title='type')
             else:
-                match = creation_column.match(elements)
+                match = creation_column.match(rows)
 
-            # Adding the column with its SQLType
-            new_column = next(oid)
+            # connecting the table with the column
             name_column = match.group('nameColumn')
-            G.add_edge(new_column, 'Column', title='type')
-            G.add_edge(new_column, name_column, title='name')
-            G.add_edge(name_elem, new_column, title='column')
+            key_column = str(name_column) + " ; " + str("Column")
+            if key_column not in elements:
+                oid_column = next(oid)
+                elements[key_column] = oid_column
+            else:
+                oid_column = elements[key_column]
+            G.add_edge(oid_column, 'Column', title='type')
+            G.add_edge(oid_column, name_column, title='name')
+            G.add_edge(oid_table, oid_column, title='column')
 
-            new_SQLType = next(oid)
-            name_SQLType = match.group('SQLType')
-            G.add_edge(new_column, new_SQLType, title='SQLType')
-            G.add_edge(new_SQLType, 'ColumnType', title='type')
-            G.add_edge(new_SQLType, name_SQLType, title='name')
+            # connecting the column with its type
+            name_SQLType = match.group('SQLtype')
+            key_SQLType = str(name_SQLType) + " ; " + str("SQLtype")
+            if key_SQLType not in elements:
+                oid_SQLType = next(oid)
+                elements[key_SQLType] = oid_SQLType
+            else:
+                oid_SQLType = elements[key_SQLType]
+            G.add_edge(oid_SQLType, 'ColumnType', title='type')
+            G.add_edge(oid_SQLType, name_SQLType, title='name')
+            G.add_edge(oid_column, oid_SQLType, title='SQLtype')
 
     sgu.schema_graph_draw(G)
+
 
 def main():
     import networkx as nx
