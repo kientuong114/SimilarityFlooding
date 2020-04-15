fdfc5da1c027a6eb9dc948bea4add58b7ae29e79
Stefan Djokovic <ste.djokovic@gmail.com>
2020-04-15 18:38:57 +0200

    refactor: improved code readability, added comments

--- similarityflooding/initial_map.py
+++ similarityflooding/initial_map.py
@@ -1,9 +1,7 @@
-import networkx as nx
 from fuzzywuzzy import fuzz
 
+
 def generate(G, H):
-    #print(G.nodes)
-    #print(H.nodes)
 
     # to exclude OIDs only checks if it starts with '&', will improve later
     # initialMap is generated with fuzzywuzzy for now, will find better alternatives later
@@ -15,9 +13,6 @@ def generate(G, H):
                     val = fuzz.ratio(g_node.lower(), h_node.lower())
                     initial_map.append([g_node, h_node, val / 100])
 
-    initial_map.sort(key=lambda x: x[2], reverse=True)      # sorting by val
-    # print("printing initialMap generated with fuzzywuzzy")
-    # for i in initial_map:
-    #     print(i)
+    initial_map.sort(key=lambda x: x[2], reverse=True)      # sorting by val, no need for this
 
     return initial_map
