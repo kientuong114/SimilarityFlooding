db553f3bc3314c4f2a4f87ed3603782c6055abe8
JohnSmith114 <kientuong@gmail.com>
2020-04-16 23:17:51 +0200

    feat: add graph traversal generators for utilities

--- similarityflooding/_schema_graph_utils.py
+++ similarityflooding/_schema_graph_utils.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from collections import deque
 import pylab
 
 
@@ -8,6 +9,43 @@ def OID_generator(char = 'a', start_num = 1):
         yield char + str(n)
         n += 1
 
+def BFS(G, startNode=None):
+    if not startNode:
+        startNode = list(G.nodes())[0]
+
+    q = deque(startNode)
+    visited = set()
+    visiting = set(startNode)
+
+    while q:
+        curr = q.popleft()
+        visiting.remove(curr)
+        yield curr
+        visited.add(curr)
+        for child in G.neighbors(curr):
+            if child not in visited and child not in visiting:
+                visiting.add(child)
+                q.append(child)
+
+def DFS(G, startNode=None):
+    if not startNode:
+        startNode = list(G.nodes())[0]
+
+    q = deque(startNode)
+    visited = set()
+    visiting = set(startNode)
+
+    while q:
+        curr = q.pop()
+        visiting.remove(curr)
+        yield curr
+        visited.add(curr)
+        for child in G.neighbors(curr):
+            if child not in visited and child not in visiting:
+                visiting.add(child)
+                q.append(child)
+
+
 
 def schema_graph_draw(G, title='title'):
     pos = nx.spring_layout(G, k=3)
@@ -15,5 +53,3 @@ def schema_graph_draw(G, title='title'):
     nx.draw(G, pos, with_labels=True, font_weight='bold')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     pylab.show()
-
-
