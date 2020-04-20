2cff50b1423d3bdd94b09803eea8a6aba7f04238
Stefan Djokovic <ste.djokovic@gmail.com>
2020-04-20 10:58:32 +0200

    docs: added docs

--- similarityflooding/initial_map.py
+++ similarityflooding/initial_map.py
@@ -1,16 +1,36 @@
 from fuzzywuzzy import fuzz
 
 
-def generate(G, H):
+def generate(G1, G2):
+    """This method generates the bare similarity values (initial_map) for all the nodes combination of the
+    two input graphs
 
-    # to exclude OIDs only checks if it starts with '&', will improve later
-    # initialMap is generated with fuzzywuzzy for now, will find better alternatives later
-    initial_map = {}
-    for g_node in G.nodes:
+    It is used to calculate the initial SF values of each node, from which then iterate through the algorithm
+
+    The initial similarity values can range from 0 (minimum) to 1 (maximum)
+
+    NOTES: currently it excluded all the OID from the initial values generations, and uses fuzzywuzzy in its
+    most simplistic way to generate the similarities. Also, as a test, we included the "string" and "varchar"
+    similarity as the maximum similarity possible (1), while the rest are computed with fuzzywuzzy.
+
+    Args:
+        G1: first input graph
+        G2: second input graph
+
+    Returns:
+        IM: initial_map
+    """
+
+    IM = {}
+    for g_node in G1.nodes:
         if g_node[0] != '&':
-            for h_node in H.nodes:
+            for h_node in G2.nodes:
                 if h_node[0] != '&':
-                    val = fuzz.ratio(g_node.lower(), h_node.lower())
-                    initial_map[tuple([g_node, h_node])] = val / 100
+                    # In the first if we can add the options of most common values that we consider similar/identical
+                    if g_node == "string" and h_node[0:7] == "varchar":
+                        IM[tuple([g_node, h_node])] = 1
+                    else:
+                        val = fuzz.ratio(g_node.lower(), h_node.lower())
+                        IM[tuple([g_node, h_node])] = val / 100
 
-    return initial_map
+    return IM
