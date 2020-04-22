669ed365ad4d643277d63f23118fccd402d246e7
Stefan Djokovic <ste.djokovic@gmail.com>
2020-04-22 11:12:03 +0200

    update: added test_base which uses example Figure 3 of the paper

--- similarityflooding/test_runs.py
+++ similarityflooding/test_runs.py
@@ -5,6 +5,7 @@ from xml_parser import parse_xml
 import induced_propagation_graph as ipg
 import filter as f
 import _schema_graph_utils as sgu
+import networkx as nx
 
 
 def test_on_sql():
@@ -13,6 +14,21 @@ def test_on_sql():
     sf = gen_sf(G1, G2)
     pairs = f.select_filter(sf)
     print(sgu.combine_oid_to_name_pairs(G1, G2, pairs))
+
+
+def test_base():
+    G1 = nx.DiGraph()
+    G1.add_edge("&a", "&a1", title='l1')
+    G1.add_edge("&a", "&a2", title='l1')
+    G1.add_edge("&a1", "&a2", title='l2')
+    G2 = nx.DiGraph()
+    G2.add_edge("&b", "&b1", title='l1')
+    G2.add_edge("&b", "&b2", title='l2')
+    G2.add_edge("&b2", "&b1", title='l2')
+
+    sf = gen_sf(G1, G2)
+    pairs = f.select_filter(sf, verbose=False)
+    print(pairs)
 
 
 def test_on_xml():
@@ -25,10 +41,8 @@ def test_on_xml():
 
 def gen_sf(G1, G2):
     sf = ipg.SFGraphs(G1, G2)
-    ipg.similarityFlooding(sf, max_steps=1000, verbose=False, fixpoint_formula=ipg.fixpoint_incremental)
+    ipg.similarityFlooding(sf, max_steps=1000, verbose=True, fixpoint_formula=ipg.fixpoint_incremental)
     return sf
-
-
 
 
 if __name__ == "__main__":
